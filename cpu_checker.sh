ecfaf43fb392b49b70e1d0a4e5b6acf8117d652e
tux <tux@nunux>
2020-07-29 04:55:57 +0200

    fix cpu_checker

--- cpu_checker.sh
+++ cpu_checker.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
+sleep 5
 MAX_CPU=7
 
-n=`w|head -1|awk '{print $10}' |sed 's/,.*//'|sed 's/\..*//'`
+# get average CPU load in last minute
+n=0`w|head -1|sed 's/.*average:\(.*\)/\1/'|awk '{print $1}'|sed 's/,.*//'|sed 's/\..*//'`
+# remove separator
+n=`echo ${n::-1}`
+# cast to int
+n=${n%.*}
 echo $n
 
 if test $n -gt $MAX_CPU
